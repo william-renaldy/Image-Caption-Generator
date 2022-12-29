e1082f65e496a1f82242b0192de114ad394c7f32
William Renaldy <williamrenaldy.a@gmail.com>
2022-12-29 14:31:09 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+from processor import GenerateDescription
+
+generator = GenerateDescription()
+
+st.set_page_config(page_title="Image Caption Generator")
+
+st.title("Image Caption Generator")
+
+
+image = st.file_uploader("Upload your Image: ")
+
+if image:
+    st.image(image)
+    caption = generator.generate(image)
+    caption = caption.strip("start")
+    caption = caption.strip("end")
+    caption = caption.strip()
+    caption = caption.capitalize()
+    caption += "."
+
+
+    st.markdown(caption)
